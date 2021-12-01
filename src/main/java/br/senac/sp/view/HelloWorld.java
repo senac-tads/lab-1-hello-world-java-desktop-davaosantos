dc4c7e0c8bf9b88b0da6c6d793db7685345be6f4
davaosantos <74523544+davaosantos@users.noreply.github.com>
2021-12-01 18:17:51 -0300

    Add files via upload
    Lab 1 feito. 
    Adicionado botao ("Clique aqui") com uma mensagem "Ola mundo" para o usuario

---
+++ src/main/java/br/senac/sp/view/HelloWorld.java
@@ -0,0 +1,19 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author david
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "Hello world");
+    }
+         
+         
+}
